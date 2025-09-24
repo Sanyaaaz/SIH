7d4f665410957ea4dc9e081480cc02ef3009a064
Saniya <saniyaarora2908@gmail.com>
2025-09-24 12:53:58 +0530

    added image

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/IH', {
   .then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.error(err));
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
